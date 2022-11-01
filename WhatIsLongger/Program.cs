0dc02578999ade854c5a9fed7f076badb7ffb739
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-11-01 15:42:43 +0200

    WhatLonger
    WhatLonger

---
+++ WhatIsLongger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WhatIsLongger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name1;
+            String name2;
+            name1 = Console.ReadLine();
+            name2 = Console.ReadLine();
+            if (name1.Length > name2.Length)
+            {
+                Console.WriteLine(name1);
+            }
+            else
+            {
+                Console.WriteLine(name2);
+            }
+        }
+    }
+}
